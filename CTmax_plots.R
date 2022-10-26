d1fab719bd3ac42ef06c0e55b23e2bcc787a2c1d
Alexandra Hahn <114489481+HahnAlexandra@users.noreply.github.com>
2022-10-26 15:53:05 +0200

    Add files via upload
    additional plots, visualization optimized

--- CTmax_plots.R
+++ CTmax_plots.R
@@ -55,12 +55,35 @@ ggplot(poster, aes(x=date_sampling, y=Ctmax, fill=treatment))+
   scale_y_continuous(limits = c(23,36), n.breaks = 5)+
   xlab("Sampling time")+ ylab("Critical thermal maximum in °C")+
   theme(legend.position = "right")+
-  scale_fill_manual(values = c("#96B48E","#CFD4EB","#D5968F"), name = "treatment")
+  scale_fill_manual(values = c("#96B48E","#CFD4EB","#D5968F"), name = "Treatment")
 
+#Ctmax - outliers removed
+ggplot(data, aes(x=date_sampling, y=Ctmax, fill=treatment))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_point(position = position_jitterdodge(jitter.width = 0.2))+
+  theme_light(base_size = 14)+
+  scale_x_discrete(labels=c("2022-04-06" = "April", "2022-05-16" = " May", "2022-06-27" = " June" , "2022-07-19" = "July"))+
+  scale_y_continuous(limits = c(23,31), n.breaks = 5)+
+  xlab("Sampling time")+ ylab("Critical thermal maximum in °C")+
+  theme(legend.position = "right")+
+  scale_fill_manual(values = c("#96B48E","#CFD4EB","#D5968F"), name = "Treatment")
+
+
+#Ctmax developmental temperature (+temp gradient)
 ggplot(data, aes(x = mean2, y = Ctmax, fill = X2.week_mean))+
   geom_boxplot(outlier.shape = NA)+
   theme_light(base_size = 14)+
   scale_fill_gradientn(colors = wes_palette("Zissou1", type = "continuous"))+
+  scale_x_discrete(labels =c("6.36" = "6.36 °C", "6.36" = "6.36 °C", "10.8" = "10.8 °C","11.44" = "11.44 °C","16.55" = "16.55 °C","17.9" = "17.9 °C" ,"18.11" = "18.11 °C"))+
+  xlab("")+ ylab("Critical thermal maximum in °C")+
+  labs(fill = "Developmental temperature in °C")+
+  theme(legend.position = "bottom")
+
+#Ctmax developmental temperature (+treatment)
+ggplot(data, aes(x = mean2, y = Ctmax, fill = treatment))+
+  geom_boxplot(outlier.shape = NA)+
+  theme_light(base_size = 14)+
+  scale_fill_manual(values = c("#96B48E","#CFD4EB","#D5968F"), name = "Treatment")+
   scale_x_discrete(labels =c("6.36" = "6.36 °C", "6.36" = "6.36 °C", "10.8" = "10.8 °C","11.44" = "11.44 °C","16.55" = "16.55 °C","17.9" = "17.9 °C" ,"18.11" = "18.11 °C"))+
   xlab("")+ ylab("Critical thermal maximum in °C")+
   labs(fill = "Developmental temperature in °C")+
@@ -70,11 +93,19 @@ ggplot(data, aes(x = mean2, y = Ctmax, fill = X2.week_mean))+
 ggplot(poster,aes(x=treatment, y=Ctmax, fill=sex_confirmed )) +
   geom_boxplot(outlier.shape = NA) +
   geom_point(position=position_jitterdodge(jitter.width=0.2))+
-  facet_wrap(~date_sampling)+
+  facet_wrap(~date_sampling,
+             labeller = labeller(date_sampling = 
+                                   c("2022-04-06" = "Collection 1 - 06 April 2022",
+                                     "2022-05-16" = "Collection 2 - 16 May 2022",
+                                     "2022-06-27" = "Collection 4 - 27 June 2022",
+                                     "2022-07-19" = "Collection 5 - 19 July 2022")))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))+
   theme_light(base_size = 14)+
   theme(legend.position = "right")+
+  theme(strip.background =element_rect(fill="white"))+
+  theme(strip.text = element_text(colour = 'black'))+
+  ylab(expression("CT"["max"]* " in °C"))+xlab("Treatment")+
   scale_fill_manual(values = c("#D5968F","#CFD4EB"), name = "sex")
 
 
@@ -100,16 +131,17 @@ p_mean <- ggplot(assays,aes(x=daily_mean,
                        y=Ctmax,
                        color=sex_confirmed,
                        fill = sex_confirmed,
-                       group = group)) +
+                       group = group_mean)) +
   theme_light(base_size = 14)+
-  ylab("Critical thermal maximum in °C")+
+  ylab(expression("CT"["max"]* " in °C"))+
   xlab("Treatment temperature in °C")+
-  theme(legend.position = "right")+
+  theme(legend.position = "none")+
   scale_color_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   scale_fill_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   geom_point(aes(shape = generation), size = 2.75)
 #"#FF8275","#8BA4FF" for darker colours
-p_mean 
+
+p_mean # plot for daily mean temperature
 pm <- p_mean + geom_smooth(method = "lm", col ="grey", aes(group = sex_confirmed))
 
 group_1w <- interaction(assays$X1.week_mean,assays$sex_confirmed)#with 1-week mean
@@ -117,37 +149,45 @@ p_1w <- ggplot(assays,aes(x=X1.week_mean,
                             y=Ctmax,
                             color=sex_confirmed,
                             fill = sex_confirmed,
-                            group = group)) +
+                            group = group_1w)) +
   theme_light(base_size = 14)+
-  ylab("Critical thermal maximum in °C")+
+  ylab(expression("CT"["max"]* " in °C"))+
   xlab("Treatment temperature in °C")+
-  theme(legend.position = "right")+
+  theme(legend.position = "none")+
   scale_color_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   scale_fill_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   geom_point(aes(shape = generation), size = 2.75)
 
-p_1w
+p_1w # plot for 1 week mean temperature
 p1 <- p_1w + geom_smooth(method = "lm", col ="grey", aes(group = sex_confirmed))
+p1
+
+library(cowplot)
+legend <- get_legend(p1)
 
 group_2w <- interaction(assays$X2.week_mean,assays$sex_confirmed)#with 2-week mean
 p_2w <- ggplot(assays,aes(x=X2.week_mean, 
                           y=Ctmax,
                           color=sex_confirmed,
                           fill = sex_confirmed,
-                          group = group)) +
+                          group = group_2w)) +
   theme_light(base_size = 14)+
-  ylab("Critical thermal maximum in °C")+
+  ylab(expression("CT"["max"]* " in °C"))+
   xlab("Treatment temperature in °C")+
-  theme(legend.position = "right")+
+  theme(legend.position = "none")+
   scale_color_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   scale_fill_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   geom_point(aes(shape = generation), size = 2.75)
 
-p_2w
+p_2w # plot for 2 week mean temperature
 p2 <- p_2w + geom_smooth(method = "lm", col ="grey", aes(group = sex_confirmed))
 
 library(gridExtra)
 grid.arrange(pm, p1, p2)
+
+combined_plot <- grid.arrange(pm, p1, p2, ncol = 1, nrow = 3)
+plot_grid(combined_plot, legend, ncol = 2 , rel_widths = c(3/4, 1/4))
+
 
 #males and females - length
 ggplot(assays, aes(y = length, x = Ctmax, col = sex_confirmed))+
@@ -176,14 +216,14 @@ ggplot(assays, aes(y = length, x = X2.week_mean, col = sex_confirmed))+
   scale_fill_manual(values = c("#D5968F","#CFD4EB"), name = "sex")+
   scale_x_continuous()+
   theme_light(base_size = 14)+
-  xlab("Acclimation temperature")+
+  xlab("Developmental temperature in °C")+
   ylab("Prosome length in µm")
 
 
 #SST and wild CTmax
 
 sub <- read.csv("~/RStuff/masterarbeit/temperature/sub.csv", header = TRUE)
-mycolors <- c("kimocc" = "#D5968F", "ctmax"= "#178B76")
+mycolors <- c("kimocc" = "#D5968F", "ctmax"= "#689C87")##64885A#178B76##94B38B##689C87
 sub$Date <-  as.Date(sub$Date)
 
 Sys.setlocale("LC_TIME", "English")#set locale to English to avoid German months
@@ -192,7 +232,7 @@ ggplot(sub, aes(x=Date, y=Temperature, group=Origin, color=Origin)) +
   geom_point() +
   theme_light(base_size = 14)+ 
   xlab("")+
-  scale_y_continuous(name="Sea Surface Temperature in °C", sec.axis = sec_axis(~ 0.58*.+20, name="Critical thermal maximum in °C")) +
+  scale_y_continuous(name="SST in °C", sec.axis = sec_axis(~ 0.58*.+20, name=expression("CT"["max"]* " in °C"))) +
   scale_color_manual(name="Origin", values = mycolors) +
   theme(
     axis.title.y = element_text(color = mycolors["kimocc"]),
